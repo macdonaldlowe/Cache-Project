ad7b0086f335b7af4009c6aca9abb63953f813ce
macdonaldlowe <macondonaldlowe@u.boisestate.edu>
2021-09-10 19:49:03 -0600

    Checkpoint before doing delimeter

--- Cache.java
+++ Cache.java
@@ -1,12 +1,143 @@
+import java.util.*;
+
 //Use import linked list
 //This will be a generic type file 
+public class Cache<T> {
+
+    private Boolean isSecond = false;
+    private int size;
+    private int secondSize;
+    private LinkedList<T> firstCache;
+    private LinkedList<T> secondCache;
+    private int firstCacheHits = 0;
+    private int firstCacheReferences = 0;
+    private int secondCacheHits = 0;
+    private int secondCacheReferences = 0;
 
 //Constructor for single cache - takes in size 
-
+public Cache(int size){
+    firstCache = new LinkedList<T>();
+    this.size = size;
+}
 //Constructor for double cache - takes in size
+public Cache(int size, int secondSize){
+    firstCache = new LinkedList<T>();
+    secondCache = new LinkedList<T>();
+    this.isSecond = true;
+    this.size = size;
+    this.secondSize = secondSize;
+}
 
-//getObject
 //addObject
+public void addObjectFirst(T object){
+    if(firstCache.size() >= size){
+        firstCache.removeLast();
+    }
+    firstCache.addFirst(object);
+}
+
+public void addObjectSecond(T object){
+    if(secondCache.size() >= size){
+        secondCache.removeLast();
+    }
+    secondCache.addFirst(object);
+}
+
+//getObject - returns null if not found
+public T getObjectFirst(T object){
+    for(T item : firstCache){
+        if(item.equals(object)){
+            return object;
+        }
+    }
+    return null;
+}
+
+//getObjectSecond - returns null if not found
+public T getObjectSecond(T object){
+    for(T item : secondCache){
+        if(item.equals(object)){
+            return object;
+        }
+    }
+    return null;
+}
+
 //removeObject
+public void removeObjectFirst(T object){
+    for(T item : firstCache){
+        if(item.equals(object)){
+            firstCache.remove(item);
+        }
+    }
+}
+
+public void removeObjectSecond(T object){
+    for(T item : firstCache){
+        if(item.equals(object)){
+            firstCache.remove(item);
+        }
+    }
+}
 //clearCache
+public void clearCacheFirst(){
+    firstCache.clear();
+}
+
+public void clearCacheSecond(){
+    secondCache.clear();
+}
+
+public String toStringFirst(){
+    String outputString = "";
+    for(T item : firstCache){
+        outputString += item.toString() + " ";
+    }
+    return outputString;
+}
+
+public String toStringSecond(){
+    String outputString = "";
+    for(T item : secondCache){
+        outputString += item.toString() + " ";
+    }
+    return outputString;
+}
+
 //methods for hit ratios getting and setting
+
+public void incrementFirstCacheHits(){
+    firstCacheHits++;
+}
+
+public int getFirstCacheHits(){
+    return firstCacheHits;
+}
+
+public void incrementFirstCacheReferences(){
+    firstCacheReferences++;
+}
+
+public int getFirstCacheReferences(){
+    return firstCacheReferences;
+}
+
+public void incrementSecondCacheHits(){
+    secondCacheHits++;
+}
+
+public int getSecondCacheHits(){
+    return secondCacheHits;
+}
+
+public void incrementSecondCacheReferences(){
+    secondCacheReferences++;
+}
+
+public int getSecondCacheReferences(){
+    return secondCacheReferences;
+}
+
+
+}
+
--- CacheTest.java
+++ CacheTest.java
@@ -1,6 +1,51 @@
 public class CacheTest {
     public static void main(String[] args) {
         //Parse command line to set up 1 cache or 2 cache of certain size
+        
+        if(args.length == 3){
+            //first cache code
+            int cacheSize = Integer.parseInt(args[1]);
+            Cache<String> dataHolder = new Cache<String>(cacheSize);
+
+            //Delemit file and tokenize into individual words
+
+            /* Algorithm for 1 cache
+            Search cache1;
+            Increment the # of cache1 references;
+            if cache1 hits 
+                then Increment the # of cache1 hits;
+            Move the hit data item to the top of cache1
+            */
+            
+        }
+
+        if(args.length == 4){
+            //second cache code
+            int cacheSize1 = Integer.parseInt(args[1]);
+            int cacheSize2 = Integer.parseInt(args[2]);
+            Cache<String> dataHolder = new Cache<String>(cacheSize1, cacheSize2);
+
+
+        //Delemit file and institute given algorithm - two versions of this algorithm
+        
+        /* Algorithm for 2 cache
+        Search cache1;
+        Increment the # of cache1 references;
+        if cache1 hits
+            then Increment the # of cache1 hits;
+                Move the hit data item to the top of both cache.
+            else Search cache2;
+                Increment the # of cache2 references; 
+                if cache2 hits
+                    then Increment the # of cache2 hits;
+                        Move the hit data item to the top of cache2;
+                        Add the data item to the top of cache1;
+                    else Add the data item to the top of both cache;
+        */
+
+        }
+
+       
 
         //Delemit file and institute given algorithm - two versions of this algorithm
         //Command line will tell us if 1 cache algorithm or two cache algorithm and output
